dc316a00be7e2b5d77d74cf5cb2a6c2e72aae30f
Gowrav Vishwakarma <gowravvishwkarma@gmail.com>
2023-12-11 18:54:25 +0530

    Autoselect where false implemented again

--- components/AutoSelect.vue
+++ components/AutoSelect.vue
@@ -47,9 +47,11 @@ const defaultGetModelOptionsFn = (val: string): ModelOptions => {
         : props.include || [],
     limit: props.limit || 10,
   };
-  modelOptions.where = props.where
-    ? props.where(val)
-    : { [props.labelFieldName]: { $like: `%${val}%` } };
+  if (props.where !== false && props.where !== undefined) {
+    modelOptions.where = props.where
+      ? props.where(val)
+      : { [props.labelFieldName]: { $like: `%${val}%` } };
+  }
 
   console.log('modelOptions', modelOptions, 'props', props);
   return modelOptions;
